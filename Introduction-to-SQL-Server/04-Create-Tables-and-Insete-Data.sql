4bedfedb8285148d511154821bd1d67b27e19b6e
Vitor Borges Jr <Vitor>
2020-09-15 13:35:58 -0300

    Update 04-Create-Tables-and-Insete-Data.sql

--- Introduction-to-SQL-Server/04-Create-Tables-and-Insete-Data.sql
+++ Introduction-to-SQL-Server/04-Create-Tables-and-Insete-Data.sql
@@ -88,4 +88,107 @@ SELECT
 FROM
   tracks;
 
--- 
+-- Select the title column from the album table where the album_id is 213.
+-- Select the album
+SELECT 
+  title 
+FROM 
+  album 
+WHERE 
+  album_id = 213;
+-- UPDATE the title to 'Pure Cult: The Best of The Cult'
+UPDATE 
+  album
+-- SET the new title    
+SET 
+  title = 'Pure Cult: The Best Of The Cult'
+WHERE album_id = 213;
+-- Run the query again
+SELECT 
+  title 
+FROM 
+  album;
+
+-- DELETE
+-- DELETE the record from album where album_id is 1
+-- Run the query
+SELECT 
+  * 
+FROM 
+  album 
+  -- DELETE the record
+DELETE FROM 
+album 
+WHERE 
+  album_id = 1 
+  -- Run the query again
+SELECT 
+  * 
+FROM 
+  album;
+  
+-- Variables and temporary tables
+-- DECLARE and SET a variable
+-- DECLARE the variable @region, which has adata tyoe of VARCHAR and length of 10
+DECLARE @region varchar(10)
+
+-- SER the variable value to 'RFC'
+SET @region = 'RFC'
+
+SELECT description,
+       nerc_region,
+       demand_loss_mw,
+       affected_customers
+FROM grid
+WHERE nerc_region = @region;
+
+-- Declare a new variable called @start of type DATE
+DECLARE @start DATE
+
+-- SET @start to '2014-01-24'
+SET @start = '2014-01-24'
+
+-- Declare a new variable called @stop of type DATE
+DECLARE @stop DATE
+
+-- SET @stop to '2014-07-02'
+SET @stop = '2014-07-02'
+
+-- Declare a new variable called @affected of type INT
+DECLARE @affected INT
+
+-- Set @affected to 5000
+SET @affected = 5000 
+
+-- Retrieve all rows where event_data is BETWEEN @start and @stop and affected_customers
+-- is greater than or equal to @affected
+SELECT 
+  description,
+  nerc_region,
+  demand_loss_mw,
+  affected_customers
+FROM 
+  grid
+-- Specify the date range of the event_date and the value for @affected
+WHERE event_date BETWEEN @start AND @stop
+AND affected_customers >= @affected;
+
+-- Create a Temporary Table woth the longest track from every album
+-- Create a temporary table called maxtracks. Make sure the table name starts with #.
+-- Join album to artist using atisti_id, and track to album using album_id.
+-- Run the final SELECT statement to retrieve all the columns from your new table.
+SELECT  album.title AS album_title,
+  artist.name as artist,
+  MAX(track.milliseconds / (1000 * 60) % 60 ) AS max_track_length_mins
+-- Name the temp table #maxtracks
+INTO #maxtracks
+FROM album
+-- Join album to artist using artist_id
+INNER JOIN artist ON album.artist_id = artist.artist_id
+-- Join track to album using album_id
+INNER JOIN track ON album.album_id = track.album_id
+GROUP BY artist.artist_id, album.title, artist.name,album.album_id
+-- Run the final SELECT query to retrieve the results from the temporary table
+SELECT album_title, artist, max_track_length_mins
+FROM  #maxtracks
+ORDER BY max_track_length_mins DESC, artist;
